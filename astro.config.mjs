843d2d39e347fa5793248e85d1c85eac5f0ee23f
Radek Dobrovolný <radekdobrovolny@gmail.com>
2026-02-10 22:30:31 +0100

    DB calling fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,4 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://radekdobrovolny.github.io',
   base: process.env.NODE_ENV === 'production' ? '/hejnice' : '/',
-  output: 'static',
 });
