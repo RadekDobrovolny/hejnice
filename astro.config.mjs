2a9708b639a85d55e2368925cc5651e9a04b5373
Radek Dobrovolný <radek.dobrovolny@gmail.com>
2026-08-05 13:54:43 +0200

    Vlastní doména

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://radekdobrovolny.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/hejnice' : '/',
+  site: 'https://setkani.hippou.cz',
 });
